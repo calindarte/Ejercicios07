6a0e2ca6fdf6c9c8ef9c70ffa4d7951d1ffe90b0
Carlos Santos <Carlos Santos@LAPTOP-H2K9SUQI>
2022-03-12 21:42:04 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicios07
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            /* recorre los numeros del 1 al 100*/
+            int i = 1;
+            Console.WriteLine("los numeros del 1 al 100");
+            while (i <= 100)
+            {
+                Console.Write(i + "  ");
+                i++;
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
